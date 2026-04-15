c8ebf74981895842653d8533764bafe568e9c7ed
isaackatana <mrisaackatana@gmail.com>
2026-04-15 23:01:03 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import './App.css'
 
-export default function App() {
+function App() {
+
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <Header />
-      <main className="p-4">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+    <>
+    </>
+  )
 }
+
+export default App
