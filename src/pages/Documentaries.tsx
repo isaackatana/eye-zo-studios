654856f6978a4bacc14336c5518a68e9ee586792
isaackatana <mrisaackatana@gmail.com>
2026-04-15 23:34:12 +0300

    1

--- src/pages/Documentaries.tsx
+++
@@ -1,8 +0,0 @@
-export default function Documentaries() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Documentaries</h1>
-      <p>Dive into thought-provoking documentaries from around the world.</p>
-    </div>
-  );
-}
--- src/pages/Film.tsx
+++
@@ -1,8 +0,0 @@
-export default function Film() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Film</h1>
-      <p>Discover new films, trailers, reviews, and cinematic experiences.</p>
-    </div>
-  );
-}
